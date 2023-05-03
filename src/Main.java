bce574cc522b23c4c403462c9fa7045a64dbdc6a
Júlia Capanema <julia.capanema@univille.br>
2023-05-03 17:24:33 -0300

    commit trabalho02

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Ganho> ganho = new ArrayList<>();
+        ArrayList<Gasto> gasto = new ArrayList<>();
+
+        int opcao = 0;
+
+        while (opcao != 6){
+            System.out.println("Sistema de gestão financeira");
+            System.out.println("Selecione a opção desejada: ");
+            System.out.println("1 - Adicionar ganho");
+            System.out.println("2 - Adicionar gasto");
+            System.out.println("3 - Relatório de ganho");
+            System.out.println("4 - Relatório de gasto");
+            System.out.println("5 - Relatório mensal");
+            System.out.println("6 - Sair");
+
+
+            System.out.println("Digite a opção desejada: ");
+            Scanner scanner = new Scanner(System.in);
+            opcao = scanner.nextInt();
+
+            if (opcao == 1){
+
+                //criar os objetos das classes gasto e ganho
+                //utilizar a classe relatório para criação dos relatórios
+                //lista e atributos ??
+            }
+
+
+        }
     }
 }
---
+++ src/relatorioFinanceiro.java
@@ -0,0 +1,22 @@
+
+public class relatorioFinanceiro {
+
+    private double totalGasto;
+    private double totalGanho;
+
+    public double getTotalGasto() {
+        return totalGasto;
+    }
+
+    public void setTotalGasto(double totalGasto) {
+        this.totalGasto = totalGasto;
+    }
+
+    public double getTotalGanho() {
+        return totalGanho;
+    }
+
+    public void setTotalGanho(double totalGanho) {
+        this.totalGanho = totalGanho;
+    }
+}
